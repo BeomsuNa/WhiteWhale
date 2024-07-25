4ce3fb1c5df775586b732ecba3a37b2d2b5e656b
skqjatn6788@gmail.com <123361606+pass98@users.noreply.github.com>
2024-07-25 22:39:53 +0900

    [fix] 1. 물건등록 페이지에서 물건 상세페이지로 이동안되는 버그 수정
    판매자 상태에서 물건 클릭 시 물건페이지로 이동되지 않는 버그 수정했습니다.

--- src/components/ui/ProductCardLayOut.tsx
+++ src/components/ui/ProductCardLayOut.tsx
@@ -1,6 +1,6 @@
 import { Product } from '@/lib/utils';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 interface ProductProps {
   product: Product;
@@ -10,25 +10,23 @@ const ProductCardLayOut: React.FC<ProductProps> = ({ product }) => {
   const navigate = useNavigate();
 
   const handleEditClick = () => {
+    console.log('이동');
     navigate(`/edit/${product.id}`);
   };
 
   return (
     <div className="w-full h-1/5 flex p-5 border border-black mb-5 items-center">
-      <div
-        onClick={handleEditClick}
-        onKeyDown={handleEditClick}
-        role="presentation"
-      />
-      <img
-        src={product.imageUrl}
-        alt={product.productName}
-        className="w-1/6 h-auto px-5 "
-      />
-      <div className="w-1/6">제목 : {product.productName}</div>
-      <div className="w-2/6">설명 :{product.productDescription}</div>
-      <div className="w-1/6">가격 : {product.productPrice}</div>
-      <div className="w-1/6">수량 : {product.productQuantity}</div>
+      <Link to={`/edit/${product.id}`}>
+        <img
+          src={product.imageUrl}
+          alt={product.productName}
+          className="w-1/6 h-auto px-5 "
+        />
+        <div className="w-1/6">제목 : {product.productName}</div>
+        <div className="w-2/6">설명 :{product.productDescription}</div>
+        <div className="w-1/6">가격 : {product.productPrice}</div>
+        <div className="w-1/6">수량 : {product.productQuantity}</div>
+      </Link>
     </div>
   );
 };
