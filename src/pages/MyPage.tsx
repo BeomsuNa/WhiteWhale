f9d4eb027f422f9c0c672e6ef7f4b098217a228c
skqjatn6788@gmail.com <123361606+Bemosu@users.noreply.github.com>
2025-03-08 01:13:42 +0900

    [mean] mypage 개발중
    1.[mean] myPage의 레이아웃을 수정했습니다.

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,13 @@
 function MyPage() {
-  return <div>마이페이지입니다. 개발중 수정시작?</div>;
+  return (
+    <div className="min-w-[750px] h-auto flex flex-col items-center mx-48 mt-12 mb-36 ">
+      <div className="w-full h-48 border border-black" />
+      <div className="w-full h-[1024px]  flex flex-row ">
+        <div className="w-[40%]  border border-black" />
+        <div className="w-full border border-black" />
+      </div>
+    </div>
+  );
 }
 
 export default MyPage;
