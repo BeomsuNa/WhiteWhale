ea79f6c0f68cc74e4fcda65feb47fabb00da22ba
skqjatn6788@gmail.com <123361606+BeomsuNa@users.noreply.github.com>
2025-04-04 00:29:08 +0900

    1.[etc] 배포 오류 추정 코드 수정
    1. [etc] vercel 배포 오류 추정되는 코드 수정 중입니다.

--- src/Dummys/UseFetchDatadummy.tsx
+++ src/Dummys/UseFetchDatadummy.tsx
@@ -3,12 +3,6 @@ import { FetchProducts } from './FetchProducts';
 import { FetchProductsResult, ProductCard } from '@/lib/utils';
 import { fetchProductCardData } from '../hooks/FetchProductCardData';
 
-// export const useFetchInfiniteProducts = () => {
-//   return useInfiniteQuery<FetchProductsResult>('products', FetchProducts, {
-//     getNextPageParam: lastPage => lastPage.nextPage || undefined,
-//   });
-// };
-
 export const useFetchProductCardData = (sortOption: string) => {
   return useQuery<ProductCard[]>(['productCardData', sortOption], () =>
     fetchProductCardData(sortOption),
