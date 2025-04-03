1744805f4164e23e2230820e15217aaec24376c9
skqjatn6788@gmail.com <123361606+BeomsuNa@users.noreply.github.com>
2025-04-04 00:45:20 +0900

    1. [fix] 문제 수정확인
    1.[fix] 문제가 수정되었는 지 테스트

--- src/Dummys/UseFetchDatadummy.tsx
+++ src/Dummys/UseFetchDatadummy.tsx
@@ -3,6 +3,12 @@ import { FetchProducts } from './FetchProducts';
 import { FetchProductsResult, ProductCard } from '@/lib/utils';
 import { fetchProductCardData } from '../hooks/FetchProductCardData';
 
+// export const useFetchInfiniteProducts = () => {
+//   return useInfiniteQuery<FetchProductsResult>('products', FetchProducts, {
+//     getNextPageParam: lastPage => lastPage.nextPage || undefined,
+//   });
+// };
+
 export const useFetchProductCardData = (sortOption: string) => {
   return useQuery<ProductCard[]>(['productCardData', sortOption], () =>
     fetchProductCardData(sortOption),
