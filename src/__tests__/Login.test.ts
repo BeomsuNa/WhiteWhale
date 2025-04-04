3c5804d27bdb6fde8dbb0241c8fdc7f9a1c27c5b
skqjatn6788@gmail.com <123361606+BeomsuNa@users.noreply.github.com>
2025-04-04 15:46:06 +0900

    1.[fix] vercel 오류 복구중
    1. [fix] vercel 로그인 안되는 오류 복구중입니다.

--- src/__tests__/Login.test.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import Login from '../sections/Login/Login';
-
-test('Login', () => {
-  render(typeof Login);
-  const handleElement = screen.getAllByText('Login');
-  expect(handleElement).toBeInTheDocument();
-});
